40b30d8cf572f96b5a6619508d59cf9a9564d75c
David Mihal <eastsideski@gmail.com>
2015-01-21 23:30:21 +0000

    Create new lists

--- client/alllists.js
+++ client/alllists.js
@@ -1,6 +1,16 @@
 Template.allLists.events({
   'click .listlink' : function(){
     Session.set('currentList', Lists.findOne()._id);
+  },
+  'click #newList': function(e){
+    var id = Lists.insert({
+      title: 'New List',
+      spots: 0,
+      eventDate: null,
+      closes: null,
+      group: Meteor.user().profile.team_id
+    });
+    Session.set('currentList', id);
   }
 });
 Template.allLists.helpers({

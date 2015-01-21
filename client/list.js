fccaafd1b2e0cfa9310eb8203f5f5acf51cbf83b
David Mihal <eastsideski@gmail.com>
2015-01-21 23:34:46 +0000

    Back button

--- client/list.js
+++ client/list.js
@@ -1,3 +1,8 @@
+Template.list.events({
+  'click .back': function(){
+    Session.set('currentList',null);
+  }
+});
 Template.list.helpers({
   list: function(){
     return Lists.findOne(Session.get('currentList'));

50e669367c53c0b6bb189904d07f1ae3df5c8993
David Mihal <eastsideski@gmail.com>
2015-01-21 23:49:53 +0000

    Change list name

--- client/list.js
+++ client/list.js
@@ -1,6 +1,12 @@
 Template.list.events({
   'click .back': function(){
     Session.set('currentList',null);
+  },
+  'blur .listTitle': function(e, template){
+    var newVal = template.find('.listTitle').value;
+    if (newVal.length > 0 && newVal !== this.title){
+      Lists.update(this._id, {$set: {title: newVal}});
+    }
   }
 });
 Template.list.helpers({

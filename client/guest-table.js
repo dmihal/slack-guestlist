081020919742c5a069d018b4783ed849530c97f0
David Mihal <eastsideski@gmail.com>
2015-02-25 01:46:06 -0500

    Filter guest list

--- client/guest-table.js
+++ client/guest-table.js
@@ -1,5 +1,25 @@
+var filter = new ReactiveVar(null);
+
 Template.guestTable.helpers({
   guests: function(){
-    return Guests.find({list:this._id});
+    var query = {list: this._id};
+    var filterVal = filter.get();
+    if (filterVal){
+      var regex = new RegExp('^'+filterVal, 'i');
+      query["$or"] = [
+        {firstName: regex},
+        {lastName:  regex}
+      ];
+    }
+    return Guests.find(query);
+  }
+});
+Template.guestTable.events({
+  "keyup .filter, blur .filter": function(e, template){
+    var filterVal = template.find('.filter').value;
+    if (filterVal == ""){
+      filterVal = null;
+    }
+    filter.set(filterVal);
   }
 });
